6bae676d3d6418741dfcd217d46c27dc7580dc13
Bouba <diarraboubaca@yahoo.fr>
2023-04-05 05:00:11 +0200

    Mise en place du Menu

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+export default function Menu() {
+    return (
+        <div className="menu">
+            <ul>
+                <li>Accueil</li>
+                <li>Ajouter un techno</li>
+                <li>Liste des technos</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>Ma progession</h1>
+            <div className="">Bienvenue sur ce site qui est consacré à ma progression en programmation.</div>
+        </div>
+    )
+}
