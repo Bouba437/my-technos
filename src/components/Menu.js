63c1cb682a4e4188cbdbbb2a1b59ddaba8a24ec0
Bouba <diarraboubaca@yahoo.fr>
2023-04-09 06:55:27 +0200

    Création d'une techno

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,9 +4,9 @@ export default function Menu() {
     return (
         <div className="menu">
             <ul>
-                <li><NavLink to="/">Accueil</NavLink></li>
-                <li><NavLink to="/add">Ajouter un techno</NavLink></li>
-                <li><NavLink to="/list">Liste des technos</NavLink></li>
+                <li><NavLink to="/" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Accueil</NavLink></li>
+                <li><NavLink to="/add" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Ajouter un techno</NavLink></li>
+                <li><NavLink to="/list" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Liste des technos</NavLink></li>
             </ul>
         </div>
     )
--- src/pages/TechnoAdd.js
+++ src/pages/TechnoAdd.js
@@ -1,16 +1,42 @@
-export default function TechnoAdd() {
+import { useState } from 'react';
+
+export default function TechnoAdd(props) {
+
+    const [techno, setTechno] = useState({
+        technoname: '',
+        technocategory: '',
+        technodescription: ''
+    });
+
+    const { handleAddTechno } = props;
+
+    function handleSubmit (e) {
+        e.preventDefault();
+        handleAddTechno(techno);
+        setTechno({
+            technoname: '',
+            technocategory: '',
+            technodescription: ''
+        })
+    }
+
+    function handleChange (e) {
+        const { name, value } = e.target;
+        setTechno({...techno, [name]: value});
+    }
+
     return (
         <div className="techno-add">
             <h1>Ajouter un techno</h1>
             <div>
-                <form action="">
-                    <label htmlFor="techno-name">Nom :</label>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <label htmlFor="technoname">Nom :</label>
                     <br />
-                    <input type="text" name="techno-name" id="techno-name" />
+                    <input type="text" name="technoname" id="technoname" value={techno.technoname} onChange={ e => handleChange(e)} />
                     <br />
-                    <label htmlFor="techno-category">Catégory :</label>
+                    <label htmlFor="technocategory">Catégory :</label>
                     <br />
-                    <select name="techno-category" id="techno-category">
+                    <select name="technocategory" id="technocategory" value={techno.technocategory} onChange={ e => handleChange(e)}>
                         <option value="">Sélectionner une catégorie</option>
                         <option value="front">Front</option>
                         <option value="back">Back</option>
@@ -18,11 +44,11 @@ export default function TechnoAdd() {
                         <option value="other">Autre</option>
                     </select>
                     <br />
-                    <label htmlFor="techno-description">Description :</label>
+                    <label htmlFor="technodescription">Description :</label>
                     <br />
-                    <textarea name="techno-description" id="techno-description" cols="30" rows="10"></textarea>
+                    <textarea name="technodescription" id="technodescription" cols="30" rows="10" value={techno.technodescription} onChange={ e => handleChange(e)}></textarea>
                     <br />
-                    <input type="submit" value="Ajouter le techno" />
+                    <input type="submit" value="Ajouter le techno" className="btn" />
                 </form>
             </div>
         </div>
